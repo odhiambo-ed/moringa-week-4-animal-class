27c1d6fb59ab68411b98c209ec633f02c93a3fd4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-04-19 23:24:59 +0300

    edit tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./dist/*.html'],
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  content: ['./src/**/*.{ts,tsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
