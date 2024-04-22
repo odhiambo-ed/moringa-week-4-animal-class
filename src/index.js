585dde059e8a4bd3fcdfe7b6907de46391b5500e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-04-22 10:59:55 +0300

    import class to root index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+import Bird from './bird';
+import Cat from './Cat';
+import Dog from './Dog';
+import Fish from './Fish';
+
+const myDog = new Dog('Rex', 'Rottweiler');
+const myCat = new Cat('Cat', 'Persian');
+const myBird = new Bird('Kiwis', 'Accipitriformes');
+const myFish = new Fish('Fish', 'Starfish');
 
 function component() {
     const element = document.createElement('div');
