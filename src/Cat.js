e80295994fb75c116fd91f8367ed83f6b5b77e14
nicole <nicolekirimi123@gmail.com>
2024-04-22 09:49:56 +0300

    add cat class

---
+++ src/Cat.js
@@ -0,0 +1,16 @@
+import Animal from './animal';
+
+export default class Cat extends Animal {
+    constructor(name, breed) {
+        super(name, 'Cat');
+        this.breed = breed;
+    }
+
+    sound() {
+        return `${this.name} meows`;
+    }
+
+    wagTail() {
+        return `${this.name} does not wag tail`;
+    }
+}
