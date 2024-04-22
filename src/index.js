86afbb7286a8f559d113fb91665070d56cc019d2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-04-22 11:36:32 +0300

    edit index add style

--- src/index.js
+++ src/index.js
@@ -11,12 +11,46 @@ const myBird = new Bird('Kiwis', 'Accipitriformes');
 const myFish = new Fish('Fish', 'Starfish');
 
 function component() {
-    const element = document.createElement('div');
+    const container = document.createElement('div');
+    container.classList.add('container');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack', 'tonight'], ' ');
+    const row = document.createElement('div');
+    row.classList.add('row');
 
-    return element;
+    const col = document.createElement('div');
+    col.classList.add('col');
+
+    const card = document.createElement('div');
+    card.classList.add('card', 'mt-3');
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    // Display results of Animal methods
+    const dogResult = document.createElement('p');
+    dogResult.textContent = `Dog: ${myDog.sound()}, ${myDog.wagTail()}`;
+    cardBody.appendChild(dogResult);
+
+    const catResult = document.createElement('p');
+    catResult.textContent = `Cat: ${myCat.sound()}, ${myCat.wagTail()}`;
+    cardBody.appendChild(catResult);
+
+    const birdResult = document.createElement('p');
+    birdResult.textContent = `Bird: ${myBird.sound()}, ${myBird.fly()}`;
+    cardBody.appendChild(birdResult);
+
+    const fishResult = document.createElement('p');
+    fishResult.textContent = `Fish: ${myFish.sound()}, ${myFish.swim()}`;
+    cardBody.appendChild(fishResult);
+
+    // Append elements
+    card.appendChild(cardBody);
+    col.appendChild(card);
+    row.appendChild(col);
+    container.appendChild(row);
+
+    return container;
 }
+
 
 document.body.appendChild(component());
